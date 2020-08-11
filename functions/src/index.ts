ac6ec62ce3e4e6d316822177d33074689d3063ac
Anthony bassey <39740647+soulzidda@users.noreply.github.com>
2020-08-11 09:11:23 +0200

    commented out sending notifications for now until ready to fix and complete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions";
 
 import mailChimpFunctions from "./mailChimp/mailChimp.functions";
 import { stripeFunctions } from "./Stripe/Stripe.functions";
-import { notificationsFunctions } from "./Notifications/Notifications.functions";
+ // import { notificationsFunctions } from "./Notifications/Notifications.functions";
 import {managementFunctions} from "./Management/Management.functions";
 
 const admin = require("firebase-admin");
@@ -30,8 +30,8 @@ exports.stripe = {
 };
 
 exports.notifications = {
-  onBookedDeal: functions.firestore.document("sessions/{sessions}").onCreate(notificationsFunctions.onBookedDeal),
-  onBookedDealTest: functions.https.onCall(notificationsFunctions.bookedDealTest),
+  // onBookedDeal: functions.firestore.document("sessions/{sessions}").onCreate(notificationsFunctions.onBookedDeal),
+  // onBookedDealTest: functions.https.onCall(notificationsFunctions.bookedDealTest),
 };
 
 exports.management = {
