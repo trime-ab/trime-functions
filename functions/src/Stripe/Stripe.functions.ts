91789f8f6bea525ef3de52dbbac1bed3204e62ac
Anthony bassey <a.bassey@live.co.uk>
2020-11-25 14:28:10 +0100

    if trainer edit is updated update stripe as well

--- functions/src/Stripe/Stripe.functions.ts
+++ functions/src/Stripe/Stripe.functions.ts
@@ -346,7 +346,97 @@ class StripeFunctions {
     return stripe.customers.update(data.stripeCustomerId, { email: data.email })
   }
 
-  async updateAccountDetails() {}
+  async updateAccountDetails(data: {
+    stripeAccountId: string
+    email?: string
+    address?: Address
+    dob?: SimpleDate
+    phoneNumber?: string
+    firstName?: string
+    lastName?: string
+  }) {
+    if (data.email) {
+      await this.updateAccountEmail(data.stripeAccountId, data.email)
+    }
+    if (data.address) {
+      await this.updateAccountAddress(data.stripeAccountId, data.address)
+    }
+    if (data.dob) {
+      await this.updateAccountDob(data.stripeAccountId, data.dob)
+    }
+    if (data.phoneNumber) {
+      await this.updateAccountPhoneNumber(
+        data.stripeAccountId,
+        data.phoneNumber,
+      )
+    }
+    if (data.firstName) {
+      await this.updateAccountFirstName(data.stripeAccountId, data.firstName)
+    }
+    if (data.lastName) {
+      await this.updateAccountLastName(data.stripeAccountId, data.lastName)
+    }
+    return 'Done'
+  }
+
+  private updateAccountLastName(stripeAccountId: string, lastName: string) {
+    return stripe.accounts.update(stripeAccountId, {
+      individual: {
+        last_name: lastName,
+      },
+    })
+  }
+
+  private updateAccountFirstName(stripeAccountId: string, firstName: string) {
+    return stripe.accounts.update(stripeAccountId, {
+      individual: {
+        first_name: firstName,
+      },
+    })
+  }
+
+  private updateAccountDob(stripeAccountId: string, dob: SimpleDate) {
+    return stripe.accounts.update(stripeAccountId, {
+      individual: {
+        dob: {
+          day: dob.day,
+          month: dob.month,
+          year: dob.year,
+        },
+      },
+    })
+  }
+
+  private updateAccountEmail(stripeAccountId: string, email: string) {
+    return stripe.accounts.update(stripeAccountId, {
+      email: email,
+    })
+  }
+
+  private updateAccountAddress(stripeAccountId: string, address: Address) {
+    return stripe.accounts.update(stripeAccountId, {
+      individual: {
+        address: {
+          line1: address.line1,
+          line2: address.line2,
+          postal_code: address.postalCode,
+          city: address.city,
+          state: address.state,
+          country: address.country,
+        },
+      },
+    })
+  }
+
+  private updateAccountPhoneNumber(
+    stripeAccountId: string,
+    phoneNumber: string,
+  ) {
+    return stripe.accounts.update(stripeAccountId, {
+      individual: { phone: phoneNumber },
+      business_profile: { support_phone: phoneNumber },
+    })
+  }
 }
 
 export const stripeFunctions = new StripeFunctions()
